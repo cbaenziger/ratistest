b8cd47f6b17a2c5cae759674ae16d4ffdbcd71f2
Clay Baenziger <cbaenziger@bloomberg.net>
2018-11-17 16:21:41 -0500

    Correct run_all_tests typos

--- run_all_tests.sh
+++ run_all_tests.sh
@@ -3,13 +3,13 @@ set -e
 
 testvms="ratishddslowdown"
 
-if [[ $1 == "build" ]];
+if [[ $1 == "build" ]]; then
   # build everything
   echo "============================================"
   echo "Building the ratistest VM:"
   echo "============================================"
   vagrant up ratistest
-  vagrant package ratistest --output ratistest.box
+  vagrant package ratistest --output $(dirname ${BASH_SOURCE[0]})/ratistest.box
   vagrant suspend ratistest
 
   echo "============================================"
@@ -23,20 +23,20 @@ if [[ $1 == "build" ]];
     vagrant suspend $vm 
   done
   echo "============================================"
-  echo "Build complete
+  echo "Build complete"
   echo "============================================"
-elif [[ $1 == "clean" ]];
+elif [[ $1 == "clean" ]]; then
   echo "============================================"
-  echo "Cleaning-up all test artifacts
+  echo "Cleaning-up all test artifacts"
   echo "============================================"
   vagrant destroy ratistest || true
   for vm in $testvms; do
     vagrant destroy $vm || true
   done
   vagrant box remove ratistest
-  rm -f ratistest.box
+  rm -f $(dirname ${BASH_SOURCE[0]})/ratistest.box
   echo "============================================"
-  echo "Clean-up complete
+  echo "Clean-up complete"
   echo "============================================"
 else
   echo "$(basename $0): Usage: $(basename $0) build"
